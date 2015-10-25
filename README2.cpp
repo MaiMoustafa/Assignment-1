42f3f4c15341cf157a77177a3abeb6f601d10e21
Mai Moustafa ElBasry <mai.mostafa.salem@outlook.com>
2015-10-26 01:11:47 +0200

    question 9

---
+++ README2.cpp
@@ -0,0 +1,35 @@
+question 9
+#ifndef STOCK20_H_
+#define STOCK20_H_
+#include <string>
+class Stock
+{
+private:
+std::string company;
+int shares;
+double share_val;
+double total_val;
+void set_tot() { total_val = shares * share_val; }
+public:
+Stock(); // default constructor
+Stock(const std::string & co, long n = 0, double pr = 0.0);
+~Stock(); // do-nothing destructor
+void buy(long num, double price);
+void sell(long num, double price);
+void update(double price);
+void show()const;
+const Stock & topval(const Stock & s) const;
+int getshares ()
+{  
+  return shares;
+};
+double getshare_val()
+{ 
+  return share_val;
+};
+const std::string & getcompany()
+{ 
+  return company;
+  };
+};
+#endif
