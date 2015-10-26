efd4cb6fb32772e6677f4cce5ce34feb13081c30
Mai Moustafa ElBasry <mai.mostafa.salem@outlook.com>
2015-10-26 02:08:01 +0200

    prog question 6

---
+++ README4.cpp
@@ -0,0 +1,31 @@
+class Move
+{
+private:
+double x;
+double y;
+public:
+Move(double a = 0, double b = 0)
+{ x=a;
+  y=b;
+  }// sets x, y to a,
+
+void showmove()
+{ 
+  cout<<X;
+  cout<<y;
+}
+Move add(const Move & m) 
+{
+   double newx= m.x+this->x;
+   double newy= m.y+this->y;
+  move m1 (double newx, double newy)
+  { 
+    return m;
+  }
+}
+void reset(double a = 0, double b = 0)
+{
+  x=a;
+  y=b;
+}
+};
